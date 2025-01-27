7d2d4aaabd7235d53eba748cee0bf4d650bd1d5a
codesuraj12 <160992864+codesuraj12@users.noreply.github.com>
2025-01-27 20:47:48 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+  <Header/>
+  <Outlet/>
+  <Footer/>
+    </>
+  )
+}
+
+export default App
